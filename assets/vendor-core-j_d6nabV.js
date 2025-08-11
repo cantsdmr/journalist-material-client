2b4a391bc6fb306aa608947811b31d35afcc6fd5
Can Tasdemir <ctasdemir@nanocosmos.net>
2025-08-12 00:46:26 +0200

    Updates

--- assets/vendor-core-j_d6nabV.js
+++ assets/vendor-core-j_d6nabV.js
@@ -1,4 +1,4 @@
-import{r as s,b as oe}from"./vendor-mui-DGxEZefn.js";/**
+import{r as s,b as oe}from"./vendor-mui-BwuEHRI_.js";/**
  * @remix-run/router v1.16.1
  *
  * Copyright (c) Remix Software Inc.
