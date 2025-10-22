0d0958891b4386ca14640bb8e1b82311e1c33e40
Can Tasdemir <can.tsdmr@gmail.com>
2025-10-22 02:05:05 +0200

    Updates

--- assets/vendor-core-5yuSNjOv.js
+++ assets/vendor-core-5yuSNjOv.js
@@ -1,4 +1,4 @@
-import{r as s,b as oe}from"./vendor-mui-l_MtHS_6.js";/**
+import{r as s,b as oe}from"./vendor-mui-D9HZydze.js";/**
  * @remix-run/router v1.16.1
  *
  * Copyright (c) Remix Software Inc.
